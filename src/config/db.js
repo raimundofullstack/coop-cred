c7baf001a71e85345169a44006f758cde5795a6a
raimartinsb <raimundo.barreto@deltainf.com.br>
2025-11-21 19:31:54 -0300

    ajusts db.js

--- src/config/db.js
+++ src/config/db.js
@@ -3,11 +3,7 @@ import logger from "../config/logger.js";
 
 export async function connectDb() {
   try {
-    const MONGO_URL =
-      process.env.MONGO_URL ||
-      (process.env.NODE_ENV === "production"
-        ? "mongodb://mongo:27017/coopcred"
-        : "mongodb://localhost:27017/coopcred");
+    const MONGO_URL = process.env.MONGO_URL;
 
     await mongoose.connect(MONGO_URL);
 
